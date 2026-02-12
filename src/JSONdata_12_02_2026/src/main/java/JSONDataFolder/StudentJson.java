d34802d889e4768ec1c12ea07fb122f57c81a5ea
Kashvi Sharma <codingspace99@gmail.com>
2026-02-12 22:53:41 +0530

    JSON data

---
+++ src/JSONdata_12_02_2026/src/main/java/JSONDataFolder/StudentJson.java
@@ -0,0 +1,30 @@
+package JSONdata_12_02_2026.src.main.java.JSONDataFolder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class StudentJson {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+
+        ObjectNode student = mapper.createObjectNode();
+        student.put("name", "John Doe");
+        student.put("age", 20);
+
+        ArrayNode subjects = mapper.createArrayNode();
+        subjects.add("Java");
+        subjects.add("Spring Boot");
+        subjects.add("Jackson");
+
+        student.set("subjects", subjects);
+
+        try {
+            String jsonString = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(student);
+            System.out.println("Student JSON Object:");
+            System.out.println(jsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
